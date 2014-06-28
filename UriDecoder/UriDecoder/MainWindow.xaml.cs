7d6fe08d92c35036435738178f774813153591e7
Shemar Lindie <shemarlindie@gmail.com>
2014-06-28 22:42:50 +0000

    Initial check in.

---
+++ UriDecoder/UriDecoder/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace UriDecoder {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnDecode_Click(object sender, RoutedEventArgs e) {
+            if (txtEncodedUri.Text.Length > 0) {
+                try {
+                    string uriString = txtEncodedUri.Text;
+                    string decoded = System.Web.HttpUtility.UrlDecode(uriString);
+
+                    if (chkUnescapeUnicode.IsChecked.HasValue && chkUnescapeUnicode.IsChecked.Value) {
+                        decoded = System.Text.RegularExpressions.Regex.Unescape(decoded);
+                    }
+
+                    txtDecodedUri.Text = decoded;
+                }
+                catch (Exception ex) {
+                    txtDecodedUri.Text = "[Error] - " + ex.Message;
+                }
+                
+            }
+        }
+    }
+}
